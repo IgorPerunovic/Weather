ff9736e4be89a880569266adc870784651cde5f6
iperunovic <iperunovic@showingtime.com>
2019-02-26 09:24:48 +0100

    Add project files.

---
+++ Weather/Weather/Models/WeatherConditionsBasic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather.Models
+{
+    public abstract class WeatherConditionsBasic
+    {
+        public double Temperature { get; set; } // degrees
+        public string Conditions { get; set; } // description
+        public int Clouds { get; set; } // percent coverage
+        public string ImgSource { get; set; } // the source for the icon representing the weather
+        public string Wind { get; set; } // description of wind
+    }
+}
---
+++ Weather/Weather/Services/IDataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Services
+{
+    public interface IDataService<T>
+    {
+        Task<bool> AddItemAsync(T location);
+
+    }
+}
