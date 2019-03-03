08f600ed32b521f8372487a5fac0675fa79e4cb8
iperunovic <iperunovic@showingtime.com>
2019-03-03 15:14:10 +0100

    Handled ListViews better, now can work on all screen heights and in landscape as well

--- Weather/Weather/Views/Forecast5Days.xaml.cs
+++ Weather/Weather/Views/Forecast5Days.xaml.cs
@@ -16,5 +16,11 @@ namespace Weather.Views
 	        base.OnAppearing();
 	        ((Forecast5DaysViewModel)this.BindingContext).Get5DaysForecastCommand.Execute(null);
 	    }
+
+
+	    private void ListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+	    {
+	        this.ForecastList.SelectedItem = null; // we don't want to mark selected item, for now tapping does nothing
+	    }
     }
 }
